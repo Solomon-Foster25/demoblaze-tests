fea7ce7a1a7dec7559030362f1eb2455f2480992
Solomon Foster <solomon.foster25@gmail.com>
2026-04-17 09:53:21 -0600

    add navigation tests

---
+++ tests/navigation.spec.js
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('Browse to a category and see if products appear', async ({ page }) => {
+    await page.goto('https://www.demoblaze.com/index.html')
+    await page.getByText('Phones').click();
+
+    await expect(page.locator('.card.h-100').first()).toBeVisible();
+});
+
+test('Click into a product and see if product detail page loads with a visible title', async ({ page }) => {
+    await page.goto('https://www.demoblaze.com/index.html');
+    await page.click('.card-title');
+
+    await expect(page.locator('.name')).toBeVisible()
+});
