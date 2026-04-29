d08c5a54c3b9005f0be63957942ae38cdc241b5b
Solomon Foster <solomon.foster25@gmail.com>
2026-04-28 19:00:16 -0600

    Refactor cart tests to use beforeEach for shared setup

--- tests/cart.spec.js
+++ tests/cart.spec.js
@@ -3,7 +3,8 @@ import { LoginPage } from '../pages/LoginPage';
 import { TEST_USER } from '../test-data';
 
 test.describe('Cart', () => {
-    test('Add a product to cart', async ({ page }) => {
+
+    test.beforeEach(async ({ page }) => {
         // Login to account
 
         await page.goto('/');
@@ -28,32 +29,11 @@ test.describe('Cart', () => {
         await page.click('.btn.btn-success.btn-lg');
     });
 
-});
+    test('Add a product to cart', async ({ page }) => {
+        await expect(page.locator('.btn.btn-success.btn-lg')).toBeVisible();
+    });
 
-test.describe('Cart', () => {
     test('View cart and verify product is there', async ({ page }) => {
-        // Login to account
-
-        await page.goto('/');
-        const loginPage = new LoginPage(page);
-        await loginPage.login(TEST_USER.username, TEST_USER.password);
-
-        // Click product
-
-        await page.locator('.card-title a').first().click();
-
-        // Confirm alert appears and handle it
-
-        page.on('dialog', async dialog => {
-            expect(dialog.type()).toContain("alert");
-            expect(dialog.message()).toContain("Product added.");
-            await dialog.accept();
-        });
-        
-        // Add to cart
-
-        await page.click('.btn.btn-success.btn-lg');
-
         // Navigate to the cart
 
         await page.click('#cartur');
@@ -62,5 +42,5 @@ test.describe('Cart', () => {
 
         await expect(page.locator('.success').first()).toBeVisible();
     });
-    
+
 });
